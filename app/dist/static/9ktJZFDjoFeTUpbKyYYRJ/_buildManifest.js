9f8da1ef659e804494bcb866ff851dc6df0d5e4b
Tiago Ferreira <tiagoitferreira@gmail.com>
2021-02-26 20:22:19 -0300

    clean crappy code to overrite with another crappy code

--- app/dist/static/9ktJZFDjoFeTUpbKyYYRJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/p/[id]":[a]}}("static\u002Fchunks\u002F432012efb96614354e703c058a7f7c4590e870d8.e09df67d6e8c6acffb6b.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
